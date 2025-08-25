852baca864dfd4aa91c52dd5d83e2275a03b6656
Jacob Spizziri <jspizziri@weare5stones.com>
2025-08-25 12:31:55 -0400

    fix(ios): fix an issue where the initial call returns nil due to the storefront not being setup yet

--- ios/StoreFront.m
+++ ios/StoreFront.m
@@ -1,5 +1,4 @@
 #import "StoreFront.h"
-
 #import <StoreKit/StoreKit.h>
 
 @implementation StoreFront
@@ -8,11 +7,14 @@ RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(getStoreFront:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
     if (@available(iOS 13.0, *)) {
-        SKStorefront* skStoreFront = [SKPaymentQueue defaultQueue].storefront;
-        if(skStoreFront != nil)
-            resolve(skStoreFront.countryCode);
-        else
-            resolve(nil);
+        SKStorefront *storefront = [SKPaymentQueue defaultQueue].storefront;
+        if (storefront) {
+            resolve(storefront.countryCode);
+        } else {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [self getStoreFront:resolve reject:reject];
+            });
+        }
     } else {
         resolve(nil);
     }
